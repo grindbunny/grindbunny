1372b591b10bf340b4d82dab09f0a1c0ff2f3316
GrindBunny <grindbunny365@gmail.com>
2023-06-23 15:25:13 +0200

    First Commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+# GrindBunny (GBUN)
+
+Welcome to the official repository for GrindBunny (GBUN), the token symbolizing hard work, perseverance, and resilience. 
+
+Our mission is to develop a community-driven cryptocurrency that supports the spirit of adventure and grind. GBUN represents the relentless grind to achieve success, in the challenging yet rewarding space of cryptocurrencies.
+
+GrindBunny is an energetic, determined bunny astronaut, our mascot symbolizing our ambitious community. We're on a collective journey to the stars, powered by blockchain.
+
+For more information, visit our official website at https://grindbunny.github.io/.
---
+++ assets/js/fundraising.js
@@ -0,0 +1,135 @@
+window.onload = async function() {
+    var investBtn = document.getElementById("investBtn");
+    var ethAmount = document.getElementById("ethAmount");
+    var usdValue = document.getElementById("usdValue");
+    var claimBtn = document.getElementById("claimBtn");
+    console.log("Fundraising script loaded!")
+    // Initialize Web3
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        await ethereum.enable();
+    }
+    else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+    }
+
+    // Load your contract's ABI
+    let response = await fetch('assets/js/contractABI.json');
+    let abi = await response.json();
+
+    // Initialize your contract
+    const contractAddress = '0x82512bFa9F266CE3D6c17BC92039daB0F63710A4';
+    let contract = new web3.eth.Contract(abi, contractAddress);
+
+    // Fetch ETH price when the page loads and store it in localStorage
+    let ethUsdPrice = localStorage.getItem('ethUsdPrice');
+    if (!ethUsdPrice) {
+        fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+            .then(response => response.json())
+            .then(data => {
+                localStorage.setItem('ethUsdPrice', data.ethereum.usd);
+            });
+    }
+
+    // Function to convert ETH to USD, and disable/enable invest button
+    ethAmount.onkeyup = function() {
+        let ethUsdPrice = parseFloat(localStorage.getItem('ethUsdPrice'));
+        usdValue.textContent = "USD Value: $" + (ethUsdPrice * ethAmount.value).toFixed(2);
+
+        if (ethAmount.value <= 0) {
+            investBtn.disabled = true;
+        } else {
+            investBtn.disabled = false;
+        }
+    }
+
+    ethAmount.onchange = function() {
+        if (ethAmount.value <= 0) {
+            investBtn.disabled = true;
+        } else {
+            investBtn.disabled = false;
+        }
+    }
+
+    // Function to initiate investment transaction
+    investBtn.onclick = async function() {
+        let accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        let account = accounts[0];
+        
+        contract.methods.contribute().send({ from: account, value: web3.utils.toWei(ethAmount.value, 'ether') })
+            .on('transactionHash', function(hash){
+                console.log('transactionHash', hash);
+            })
+            .on('confirmation', function(confirmationNumber, receipt){
+                console.log('confirmation: ' + confirmationNumber);
+            })
+            .on('receipt', function(receipt){
+                console.log('receipt', receipt);
+            })
+            .on('error', console.error);
+    }
+
+        // Function to claim tokens
+        claimBtn.onclick = async function() {
+            let accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            let account = accounts[0];
+    
+            contract.methods.contributorClaimCBUNTokens().send({ from: account })
+                .on('transactionHash', function(hash){
+                    console.log('transactionHash', hash);
+                })
+                .on('confirmation', function(confirmationNumber, receipt){
+                    console.log('confirmation: ' + confirmationNumber);
+                })
+                .on('receipt', function(receipt){
+                    console.log('receipt', receipt);
+                })
+                .on('error', console.error);
+        }
+        var addTokenBtn = document.getElementById("addToken"); // Add this line at the top with your other variable declarations.
+
+        // Below Function Adds the token to the wallet, MUST BE UPDATED BEFORE OFFICIAL DEPLOYMENT!
+        addTokenBtn.onclick = async function() {
+            const tokenAddress = '0xde6B9AAaE403e038c39B7dF856Aab23dE635cE36'; // replace with your token contract address
+            const tokenSymbol = 'SUPER'; // replace with your token symbol
+            const tokenDecimals = 18; // replace with your token's decimals
+            const tokenImage = 'https://raw.githubusercontent.com/grindbunny/grindbunny/main/grindbunnytokenround.png'; // replace with the URL of your token's logo
+    
+            try {
+                const wasAdded = await ethereum.request({
+                    method: 'wallet_watchAsset',
+                    params: {
+                        type: 'ERC20',
+                        options: {
+                            address: tokenAddress,
+                            symbol: tokenSymbol,
+                            decimals: tokenDecimals,
+                            image: tokenImage,
+                        },
+                    },
+                });
+    
+                if (wasAdded) {
+                    console.log('Token was successfully added to MetaMask!');
+                } else {
+                    console.log('Token was not added to MetaMask.');
+                }
+            } catch (error) {
+                console.error('An error occurred: ', error.message);
+            }
+        };
+    //Below makes the copying of the contract ID possible
+
+    
+};
+
+async function copyContractId() {
+    var copyText = document.getElementById("contractId").value;
+    try {
+        await navigator.clipboard.writeText(copyText);
+        //alert("Contract ID copied: " + copyText);
+    } catch (err) {
+        //console.error('Failed to copy text: ', err);
+    }
+}
+
